9f99cb4e714b2d0771c3eca238427d77f1b0a43b
Mat Ryer <mat@stretchr.com>
2013-12-30 13:26:43 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+
+  version
+  by Mat Ryer
+
+  Command line tool to help manage version numbers
+  for projects and directories.
+
+  Copyright (c) 2014 Stretchr, Inc.
+
+*/
+
+func main() {
+
+}
